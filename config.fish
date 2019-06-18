735a2058431ca0006d6e71599685ad7d41d0de5f
Artem Vovsia <Artem.Nomad@gmail.com>
2019-06-18 18:03:54 +0100

    Long time no update

--- config.fish
+++ config.fish
@@ -1,50 +1,42 @@
-############################################################################################
 # ALIASES
-############################################################################################
 alias tmux="env TERM=xterm-256color tmux -2"
-
 alias l="ls -lAhG"
 alias ssh="ssh -A"
-
 alias ps="ps aux"
 
-# Emacs daemon and emacs client
-# Run in the terminal. Start daemon if not started yet
-alias e='emacsclient -t -a "" $argv'
-# Run in the existing GUI frame. Start daemon if not started yet
-alias em='emacsclient -a "" $argv >/dev/null 2>&1 &'
-# Run new GUI frame. Start daemon if not started yet
-alias ema='emacsclient -c -a "" $argv >/dev/null 2>&1 &'
+alias p="pwd"
+alias b="git status -sb"
+function v
+  if set -q VIRTUAL_ENV
+    echo $VIRTUAL_ENV
+  end
+end
 
-set -x AWS_SECRET_ACCESS_KEY fnVlh8A1jwdF9kNXL8Z0TaKJo2eMuIqBCpn+8D1a
+# SPARK, HIVE, HADOOP
+set -x PYTHONPATH $PYTHONPATH "/Users/artem.vovsia/bin/spark-2.3.1-bin-hadoop2.7/python"
+set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home"
+set -x SPARK_HOME "/Users/artem.vovsia/bin/spark-2.3.1-bin-hadoop2.7"
+set -x PATH $PATH "/Users/artem.vovsia/bin/spark-2.3.1-bin-hadoop2.7/bin"
 
-# Python
+# PYTHON
 eval (python -m virtualfish)
+set -x PYSPARK_DRIVER_PYTHON "ipython" # "jupyter"
+set -x PYSPARK_SUBMIT_ARGS "--packages com.amazonaws:aws-java-sdk:1.11.329,org.apache.hadoop:hadoop-aws:2.6.0 pyspark-shell"
+set -x PYSPARK_DRIVER_PYTHON_OPTS "'notebook'"
+
+# TOOLS
+# The next line updates PATH for the Google Cloud SDK.
+set -x PATH $PATH "/Users/artem.vovsia/.fzf/bin"
+set -x PATH $PATH "/Users/artem.vovsia/bin/google-cloud-sdk/bin"
+set -x PATH $PATH "/Applications/Postgres.app/Contents/Versions/10/bin"
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-# SPARK, HIVE, HADOOP, KERBEROS
-set -x PYTHONPATH $PYTHONPATH "/Users/artem.vovsia/bin/spark-2.2.0-bin-hadoop2.7/python" $PYTHONPATH
-set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home"
-set -x HADOOP_HOME "/Users/artem.vovsia/bin/hive_client/hadoop-2.7.5"
-set -x HADOOP_PREFIX "/Users/artem.vovsia/bin/hive_client/hadoop-2.7.5"
-set -x HADOOP_CONF_DIR "/Users/artem.vovsia/bin/hive_client/hadoop_conf"
-set -x HIVE_CONF_DIR "/Users/artem.vovsia/bin/hive_client/hadoop_conf"
-set -x SPARK_HOME "/Users/artem.vovsia/bin/spark-2.2.0-bin-hadoop2.7"
-set -x KRB_USERNAME artem.vovsia
-set -x KRB_KEYTAB /Users/artem.vovsia/bin/hive_client/krb/artem.vovsia.headless.keytab
+# PROMPT
+# Don't show anything on the right
+function fish_right_prompt
+#intentionally left blank
+end
 
-set -x PYSPARK_DRIVER_PYTHON "jupyter"
-set -x PYSPARK_DRIVER_PYTHON_OPTS "'notebook'"
-
-set -x PATH $PATH "/Users/artem.vovsia/bin/hive_client/apache-hive-1.2.2-bin/bin"
-set -x PATH $PATH "/Users/artem.vovsia/bin/hive_client/hadoop-2.7.5/bin"
-set -x PATH $PATH "/Users/artem.vovsia/bin/spark-2.2.0-bin-hadoop2.7/bin"
-set -x PATH $PATH "/Users/artem.vovsia/.fzf/bin"
-set -x PATH $PATH "/Users/artem.vovsia/bin/google-cloud-sdk/bin"
-
-export PATH="/Users/artem.vovsia/src/spark-1.6.3-bin-hadoop2.4/bin:/Users/artem.vovsia/Library/Python/3.6/bin:/Users/artem.vovsia/.fzf/bin:/Users/artem.vovsia/Downloads/google-cloud-sdk/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/artem.vovsia/src/hive_client/hadoop-2.7.4/bin"
-
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/artem.vovsia/bin/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/artem.vovsia/bin/google-cloud-sdk/path.fish.inc'; else; . '/Users/artem.vovsia/bin/google-cloud-sdk/path.fish.inc'; end; end
+# SKIMLINKS
+source ~/dotfiles/priv_skimlinks.fish
--- install.sh
+++ install.sh
@@ -5,32 +5,50 @@
 # fish shell
 # git
 # curl
-# emacs
+# # emacs
 # TODO: https://hackercodex.com/guide/mac-osx-mavericks-10.9-configuration/
 # TODO: install silversearcher(ag) for linux and OS X
 
 # Install emacs https://www.emacswiki.org/emacs/EmacsForMacOS#toc15
-brew cask install emacs
+# brew cask install emacs
 
 # Install ag search tool
 brew install the_silver_searcher
-
-# Launch emacs daemon on startup https://emacs.stackexchange.com/questions/8169/how-can-i-run-emacs-24-x-app-on-mac-os-x-in-daemon-mode
-ln -sfv /usr/local/opt/emacs/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.emacs.plist
-
-# Install fzf https://github.com/junegunn/fzf
-# Fuzzy finder for command line brew install fzf
-
-# Install z https://github.com/rupa/z
 
 # Link dot files
 cd ~/
 
 mkdir -p ~/.config/fish
-#rm .gitconfig .tmux.conf .vimrc # .config/fish/config.fish .spacemacs
 
 ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+
+########
+# FISH #
+########
+# https://github.com/JorgeBucaran/awesome-fish
+
+pip install virtualfish
+
+# Install fzf https://github.com/junegunn/fzf
+brew install fzf
+
+# Install Oh My Fish:
+curl -L https://get.oh-my.fish | fish
+
+# Install z https://github.com/rupa/z:
+# Alternative: https://github.com/jethrokuan/z
+omf install z
+
+# Install NVM(Node version manager) in Fish shell
+omf install nvm
+
+# Install Fisher(fish plugin manager)
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+# Install Bax https://github.com/jorgebucaran/fish-bax
+fisher add jorgebucaran/fish-bax
